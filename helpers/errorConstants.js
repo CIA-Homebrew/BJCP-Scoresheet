3e56deaa80a32a14c551656d509d565b48115c23
Matt Westwick <matt.westwick@gmail.com>
2020-10-12 00:02:50 -0500

    fixed issues #58, #57, #55, #48

--- helpers/errorConstants.js
+++ helpers/errorConstants.js
@@ -2,5 +2,6 @@ module.exports = Object.freeze({
     PASSWORD_FAIL_CRITERIA : "Password does not meet the required security criteria",
     INVALID_PASSWORD : "Submitted password did not match existing recorded password",
     EMAIL_FAIL_CRITERIA : "Invalid email address format",
-    INVALID_EMAIL : "Submitted email did not match existing email address record"
+    INVALID_EMAIL : "Submitted email did not match existing email address record",
+    INVALID_FLIGHT_EMPTY : "Cannot submit an empty flight"
 })
