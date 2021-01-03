59d62d74fed423332904415f09cb396a0afc9d2f
Matt Westwick <matt.westwick@gmail.com>
2021-01-03 14:31:16 -0600

    added sandbox logo to prevent confusion

--- helpers/appConstants.js
+++ helpers/appConstants.js
@@ -2,7 +2,7 @@ module.exports = Object.freeze({
   SUPERUSER_LEVEL: 90, // Level of 90 or higher is required for admin access
   HEAD_JUDEGE_LEVEL: 1,
   USER_LEVEL: 0,
-  APP_NAME: "BJCP Scoresheets",
+  APP_NAME: "BJCP Scoresheets Sandbox",
   PASSWORD_REGEX: /.*/, // Regex for checking if password is strong enough
   EMAIL_REGEX: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
 });
